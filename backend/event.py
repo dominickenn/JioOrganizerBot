43365e2aa73f906b49ce1558ffda7b43a14da500
Dominic Kenn Lim <dominickenn@gmail.com>
2022-02-12 13:07:05 +0800

    Create event class and methods

--- backend/event.py
+++ backend/event.py
@@ -1,1 +1,20 @@
-#EVENT CLASS
+import bisect
+#bisect is the optimal method for inserting into sorted list
+
+class Event:
+    def __init__(self, eventname) -> None:
+        self.eventname = eventname
+        self.dates = []
+        self.locations = []
+
+    def addDate(self, date) -> None:
+        bisect.insort(self.dates, date)
+
+    def addLocation(self, location) -> None:
+        bisect.insort(self.locations, location)
+
+    def deleteDate(self, index) -> None:
+        del self.dates[index]
+
+    def deleteLocation(self, index) -> None:
+        del self.locations[index]
