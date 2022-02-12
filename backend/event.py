0ff13004df011d69f928204ab1113c9405e7cf80
Dominic Kenn Lim <dominickenn@gmail.com>
2022-02-12 13:32:57 +0800

    Added exceptions to event class

--- backend/event.py
+++ backend/event.py
@@ -1,20 +1,38 @@
 import bisect
+import logging
 #bisect is the optimal method for inserting into sorted list
 
 class Event:
+
     def __init__(self, eventname) -> None:
         self.eventname = eventname
         self.dates = []
         self.locations = []
 
     def addDate(self, date) -> None:
-        bisect.insort(self.dates, date)
+        try:
+            bisect.insort(self.dates, date)
+            logging.info(f"Successfuly added \'{date}\' to \'{eventname}\'")
+        except:
+            logging.error(f"Failed to add \'{date}\' to \'{eventname}\'")
 
     def addLocation(self, location) -> None:
-        bisect.insort(self.locations, location)
+        try:
+            bisect.insort(self.locations, location)
+            logging.info(f"Successfuly added \'{location}\' to \'{eventname}\'")
+        except:
+            logging.error(f"Failed to add \'{location}\' to \'{eventname}\'")
 
     def deleteDate(self, index) -> None:
-        del self.dates[index]
+        try:
+            del self.dates[index]
+            logging.info(f"Successfuly deleted \'{dates[index]}\' from \'{eventname}\'")
+        except:
+            logging.error(f"Failed to delete \'{dates[index]}\' from \'{eventname}\'")
 
     def deleteLocation(self, index) -> None:
-        del self.locations[index]
+        try:
+            del self.locations[index]
+            logging.info(f"Successfuly deleted \'{location[index]}\' from \'{eventname}\'")
+        except:
+            logging.error(f"Failed to delete \'{location[index]}\' from \'{eventname}\'")
