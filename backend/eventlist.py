3d81b9a0f0de268efa1bf113719cadb2dd504813
Dominic Kenn Lim <dominickenn@gmail.com>
2022-02-12 14:10:06 +0800

    Create EventList class

---
+++ backend/eventlist.py
@@ -0,0 +1,51 @@
+from event import Event
+import logging
+
+class EventList:
+    
+    def __init__(self) -> None:
+        self.events = []
+        self.eventNames = []
+
+    def createEvent(self, eventname) -> None:
+        try:
+            self.events.append(Event(eventname))
+            self.eventNames.append(eventname)
+            logging.info(f"Successfuly created \'{eventname}\'")
+        except:
+            logging.error(f"Failed to create \'{eventname}\'")
+
+    def getEventList(self) -> list:
+        try:
+            return self.eventNames
+        except:
+            logging.error(f"Failed to retrieve event list")
+
+    def getEventDates(self, index) -> list:
+        return self.events[index].getDates()
+
+    def getEventLocations(self, index) -> list:
+        return self.events[index].getLocations()
+
+    def getEventName(self, index) -> str:
+        return self.eventNames[index]
+
+    def deleteEvent(self, index) -> None:
+        try:
+            del self.events[index]
+            del self.eventNames[index]
+            logging.info(f"Successfuly deleted \'{self.eventNames[index]}\'")
+        except:
+            logging.error(f"Failed to delete {self.eventNames[index]}")
+
+    def addDateToEvent(self, event_index, date) -> None:
+        self.events[event_index].addDate(date)
+
+    def addLocationToEvent(self, event_index, location) -> None:
+        self.events[event_index].addDate(location)
+
+    def deleteDateFromEvent(self, event_index, date) -> None:
+        self.events[event_index].deleteDate(date)
+
+    def deleteLocationFromEvent(self, event_index, location) -> None:
+        self.events[event_index].deleteDate(location)
